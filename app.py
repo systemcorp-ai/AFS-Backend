9ca0562ba2989c924ddcbfe5fab911774baccea9
GioLomia <lomiag@berea.edu>
2019-06-06 10:04:19 -0400

    m

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ def respond(sender, message):
 
 def is_user_message(message):
     """Check if the message is a message from the user"""
+    print("got the m!")
     return (message.get('message') and
             message['message'].get('text') and
             not message['message'].get("is_echo"))
